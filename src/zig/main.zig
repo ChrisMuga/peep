f9db486cf1195744d29a5403d86b0f554df3a56d
Chris Muga <chrismuga94@gmail.com>
2026-05-08 14:05:01 +0300

    dir: iterate if file is dir

--- src/zig/main.zig
+++ src/zig/main.zig
@@ -113,8 +113,13 @@ pub fn main(init: std.process.Init) !void {
             if (stat.kind == std.Io.File.Kind.directory) {
                 // TODO: List root directory entries
                 print("{s} is a directory\n", .{file_name});
-                // const dir =  try std.Io.Dir.it(cwd, io, file_name, .{.iterate = true});
-                // TODO: io.Dir.iterate is not implemented, yet - revisit
+                const dir =  try std.Io.Dir.openDir(cwd, io, file_name, .{.iterate = true});
+                defer dir.close(io);
+                var dirIterator = std.Io.Dir.iterate(dir);
+
+                while(try dirIterator.next(io))|v|{
+                   print("{s}\n", .{v.name}); 
+                }
 
                 return;
             }
